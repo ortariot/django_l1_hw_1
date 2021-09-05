b8b38d1ab34b68b49fdbe2658e20dc039e416292
ortariot <v.n.homutov@gmail.com>
2021-09-06 00:18:10 +0700

    all cases successfully completed

--- app/views.py
+++ app/views.py
@@ -1,40 +1,42 @@
-from django.http import HttpResponse
 from django.shortcuts import render, reverse
 from datetime import datetime
 import os
 
 
+def get_pages():
+    pages = {'Главная страница': reverse('home'),
+             'Показать текущее время': reverse('time'),
+             'Показать содержимое рабочей директории': reverse('workdir')
+             }
+    return pages
+
+
 def home_view(request):
     template_name = 'app/home.html'
-    pages = {
-        'Главная страница': reverse('home'),
-        'Показать текущее время': reverse('time'),
-        'Показать содержимое рабочей директории': reverse('workdir')
-    }  
     context = {'title': 'Выберите раздел',
-               'pages': pages
+               'pages': get_pages()
                }
     return render(request, template_name, context)
 
 
 def time_view(request):
     template_name = 'app/home.html'
-    current_time = datetime.now().utcnow()
-    msg = {'сейчас': current_time}
-
-    context = {'title': f'Текущее дата и время: {current_time}'}
-             
-
+    current_time = datetime.now().strftime("%Y:%m:%d - %H:%M:%S")
+    context = {'title': 'Текущее дата и время',
+               'pages': get_pages(),
+               'content': [current_time]
+               }
     return render(request, template_name, context)
 
 
 def workdir_view(request):
     template_name = 'app/home.html'
     directories_file = os.listdir()
-    msg = "<br>".join(directories_file)
+    dir_list = directories_file
 
     context = {'title': 'Список файлов в рабочей директории:',
-               'pages': directories_file
+               'pages': get_pages(),
+               'content': dir_list
                }
 
     return render(request, template_name, context)
